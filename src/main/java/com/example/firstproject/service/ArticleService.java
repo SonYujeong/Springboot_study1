fa48d0e6371d1b02416b13f5795355f810ead916
Yujeong Son <yujeongs24@naver.com>
2022-03-23 16:28:07 +0900

    transaction-test

--- src/main/java/com/example/firstproject/service/ArticleService.java
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -11,7 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service //서비스 선언(서비스 객체를 스프링부트에 생성한다.)
@@ -69,5 +72,36 @@ public class ArticleService {
         //3. 대상 삭제하기
         articleRepository.delete(target);
         return target;
+    }
+
+    @Transactional //해당 메소드를 트랜잭션으로 묶는다.
+    public List<Article> createArticles(List<ArticleForm> dtos) {
+        //1. dto 묶음을 entity 묶음으로 변환하기
+        List<Article> articleList = dtos.stream().map(dto->dto.toEntity()).collect(Collectors.toList());
+
+        //for문으로 작성
+        /*  List<Article> articleList = new ArrayList<>();
+          for (int i = 0; i < dtos.size(); i++){
+              ArticleForm dto = dtos.get(i);
+              Article entity = dto.toEntity();
+              articleList.add(entity);
+        }*/
+
+        //2. entity 묶음을 DB에 저장한다.
+        articleList.stream().forEach(article -> articleRepository.save(article));
+
+        //for문으로 작성
+        /*for (int i = 0; i < articleList.size(); i++){
+            Article article = articleList.get(i);
+            articleRepository.save(article);
+        }*/
+
+        //3. 강제로 예외를 발생시킨다.
+        articleRepository.findById(-1L).orElseThrow( //id는 음수가 될 수 없으므로 예외 발생
+                () -> new IllegalArgumentException("결재 실패!")
+        );
+
+        //4. 결과값 반환하기
+        return articleList;
     }
 }
